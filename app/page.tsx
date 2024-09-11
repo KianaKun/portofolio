b49b49699efc30db35de41310f5007dd060cfdd7
KianaKun <hilmananz@gmail.com>
2024-09-12 05:31:07 +0700

    fixing repeating navbar at template

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Nav from "@/app/component/nav"
-import Header from "@/app/component/nav"
+import Header from "@/app/component/header"
 import Skill from "@/app/component/skill"
 import Experience from "@/app/component/experience"
 import Portofolio from "@/app/component/portofolio"
